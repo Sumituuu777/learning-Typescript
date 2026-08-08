9c46fed13dda694e06aceacbd5c0063129847ba9
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-08-08 19:04:16 +0530

    executed first peice of code

---
+++ chapterOne/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return ` hello,${name},welcome`;
+}
+const username = "sumit";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ chapterOne/src/index.ts
@@ -0,0 +1,7 @@
+function greet(name:string):string{
+
+    return ` hello,${name},welcome`
+}
+
+const username:string="sumit"
+console.log(greet(username))
