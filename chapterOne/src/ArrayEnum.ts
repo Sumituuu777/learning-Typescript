789203c66b95c809996d452ebc7ad0e482eebe3f
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-08-11 18:43:53 +0530

    tuples,enum

--- chapterOne/src/ArrayEnum.ts
+++ chapterOne/src/ArrayEnum.ts
@@ -13,7 +13,10 @@ const menu:Chai[]=[ {name:"masala",price:10}, {name:"adrak",price:5} ]
 const cities:readonly string[]=["jaipur", "alwar"]
 // cities.push("newcity")            ye na toh push method suggest karega na kaam karne dega
 
-//---------------------------------------- Tuples -----------------------------------------------------
+
+//----------------------------------------    Tuples    -----------------------------------------------------
+
+
 let chaiTuples:[string,number,boolean?]
 
 chaiTuples=["masala",20]  // order should be same, can use optional parameters like boolean in example
@@ -24,4 +27,24 @@ let addTuple:readonly[number,number]
 let teaTuples:[name:string,price:number]                     // named tuple taki pata chale ki konsi value likhni h
 teaTuples=["adrak",20]
 
-//----------------------------------
+//Note:- tuples me .push kaam kar jata h(kyuki h toh woh array hi) or ye chiz koi error nhi deti toh debug karna hard h,avoid pushing
+
+
+//----------------------------------------     Enums      -------------------------------------------------------
+
+enum size{
+    small,
+    medium,  // anything custom smallcase or uppercase(preferred)
+    LARGE
+}
+const cupSize=size.medium
+
+//increment
+enum status{
+    PENDING=100,
+    SENT,                // agar ek value de di toh aage ki value increment hoke =101 ho jaegi
+    FAILURE              // =102
+}
+const curentStatuscode=status.FAILURE    //so basically now curentStatuscode=102
+
+//Note:-> enum hum homogenous banate h , aisa nhi enum me id,name,items sab ho. ex:-status wala pura no. ka h aise
