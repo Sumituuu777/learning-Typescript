c9629fd48a3451cccd38cbbdb12fde2d209114df
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-08-13 19:28:35 +0530

    interfaces vs gen 1

--- chapterOne/src/interfaceGenetics.ts
+++ chapterOne/src/interfaceGenetics.ts
@@ -4,3 +4,48 @@
 // class	                           ✅ Yes	         Create runtime objects/behavior
 // function	                           ✅ Yes	           Create runtime behavior
 // enum	                               ✅ Usually	     Create runtime values
+
+interface DisCountClac{
+    (price:number):number  //interface method banane ke kaam nhi aata par fir bhi karna h, aise kart sakte ho 
+}
+const apply50:DisCountClac= (p)=>p*0.5
+
+interface TeaMachine{
+    start():void,
+    stop():void
+}
+const machine:TeaMachine={            //ab ye suggestion dega ki ye 2 method toh hone hi chahiye 
+    start() {
+        
+    },
+    stop() {
+        
+    }
+}
+//---------------------------------------- index signatures(usually avoided) ----------------------------------
+interface ChaiRartings{
+    [flavor:string]:number
+}
+const ratings:ChaiRartings={
+    masala:3.7,
+    adrak:3.7,
+}
+//------------------------ interface merge ho jate h------------------------------------
+interface User{
+    name:string
+}
+interface User{
+    age:number
+}
+const u:User={
+    name:"sumit",
+    age:20
+}
+//aise bhi kar sakte h
+interface A{
+    name:string
+}
+interface B{
+    age:number
+}
+interface C extends A,B{}
